b2a90dbc463a7a25f87ebfff00355b0a1f75305b
Phùng Văn Tú <tuphung.03032004@gmail.com>
2026-08-23 15:12:05 +0700

    Sửa sai cấu hình eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,20 @@
-import js from '@eslint/js';
+// @ts-ignore
 import baseConfig from '@hono/eslint-config';
 import eslintConfigPrettier from 'eslint-config-prettier';
-import { defineConfig } from 'eslint/config';
+import { defineConfig, globalIgnores } from 'eslint/config';
 import globals from 'globals';
-import tseslint from 'typescript-eslint';
 
-export default defineConfig({
-	...tseslint.configs.recommendedTypeChecked,
-	...baseConfig,
-	...eslintConfigPrettier,
-	files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
-	plugins: { js },
-	extends: ['js/recommended'],
-	languageOptions: {
-		globals: globals.node,
-		parserOptions: {
-			allowDefaultProject: ['eslint.config.js'],
-			projectService: true
+export default defineConfig(
+	globalIgnores(['.wrangler/**', 'dist/**', 'worker-configuration.d.ts', 'eslint.config.js']),
+	baseConfig,
+	{
+		files: ['**/*.ts'],
+		languageOptions: {
+			globals: globals.node,
+			parserOptions: {
+				projectService: true
+			}
 		}
 	},
-	ignores: ['.wrangler/**', 'dist/**', 'worker-configuration.d.ts']
-});
+	eslintConfigPrettier
+);
